a2f5f88f49fab699a951f5caf086c9c66da3bde1
LuanSFMarques <luansfmarques@gmail.com>
2024-09-16 12:49:39 -0300

    .

--- matrix/matrix_numpy1.py
+++ matrix/matrix_numpy1.py
@@ -4,13 +4,16 @@ m1 = np.array([[-2,4],[1,0],[3,-1]])
 m2 = np.array([[5,0],[-1,3]])
 m3 = np.array([[2,1],[3,2]])
 
-def sum(m1:np.array, m2:np.array) -> np.array:
+def mx_sum(m1:np.array, m2:np.array) -> np.array:
     return m1+m2
 
-def multiply(m1:np.array, m2:np.array) -> np.array:
+def mx_multiply(m1:np.array, m2:np.array) -> np.array:
     return np.dot(m1,m2) # must be i[j] == [i]j
 
-def transpose(m1:np.array) -> np.array:
-    return np.transpose(m1)
+def mx_transpose(m:np.array) -> np.array:
+    return np.transpose(m1) # i -> j, j -> i
 
-print(sum(m2,m3))
+def mx_determinant(m:np.array) -> np.array:
+    return np.linalg.det(m)
+
+print(mx_sum(m2,m3))
